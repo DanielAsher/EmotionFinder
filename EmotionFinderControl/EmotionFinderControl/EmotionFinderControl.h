83bfdd1bfbc2e170514bcad2ed5d72766ff5398f
Daniel <daniel.asher.2303@gmail.com>
2015-04-11 20:09:24 +0100

    Initial commit.

---
+++ EmotionFinderControl/EmotionFinderControl/EmotionFinderControl.h
@@ -0,0 +1,19 @@
+//
+//  EmotionFinderControl.h
+//  EmotionFinderControl
+//
+//  Created by Daniel Asher on 11/04/2015.
+//  Copyright (c) 2015 AsherEnterprises. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EmotionFinderControl.
+FOUNDATION_EXPORT double EmotionFinderControlVersionNumber;
+
+//! Project version string for EmotionFinderControl.
+FOUNDATION_EXPORT const unsigned char EmotionFinderControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmotionFinderControl/PublicHeader.h>
+
+
