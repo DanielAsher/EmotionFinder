966a6d8c97e9befe4b1e033f49e9ef19443901fb
Daniel <daniel.asher.2303@gmail.com>
2015-04-12 00:33:03 +0100

    add circle of topLevelEmotionLabels around centreEmotionLabel

--- EmotionFinderControl/EmotionFinderControl/EmotionFinderView.swift
+++ EmotionFinderControl/EmotionFinderControl/EmotionFinderView.swift
@@ -8,12 +8,14 @@
 
 import UIKit
 import Cartography
+import Darwin
 
 @IBDesignable
 public class EmotionFinderView: UIControl {
     let centreEmotion = "Calm"
     let topLevelEmotions = ["Peaceful", "Mad", "Sad", "Scared", "Joyful", "Powerful"]
     let centreEmotionLabel : LTMorphingLabel
+    var topLevelEmotionLabels : [String: LTMorphingLabel] = [:]
     override init(frame: CGRect) {
         centreEmotionLabel = LTMorphingLabel()
         super.init(frame: frame)
@@ -29,16 +31,36 @@ public class EmotionFinderView: UIControl {
     func setup() {
         centreEmotionLabel.text = centreEmotion
         self.addSubview(centreEmotionLabel)
+        for emotion in topLevelEmotions {
+            let label = LTMorphingLabel()
+            label.text = emotion
+            self.addSubview(label)
+            topLevelEmotionLabels[emotion] = label
+        }
     }
     
     public override func layoutSubviews() {
         if needsUpdateConstraints() {
-            let size = self.centreEmotionLabel.intrinsicContentSize()
+            let centreSize = self.centreEmotionLabel.intrinsicContentSize()
             
             layout(centreEmotionLabel, self) { label, view in
                 label.center == view.center
-                label.width == size.width
-                label.height == size.height
+                label.width == centreSize.width
+                label.height == centreSize.height
+            }
+            
+            var incr = 2 * M_PI / Double(topLevelEmotions.count)
+
+            for (i, (emotion, label)) in enumerate(topLevelEmotionLabels) {
+                let size = label.intrinsicContentSize()
+                let angle = Double(i) * incr
+                layout(centreEmotionLabel, label) { centreLabel, label in
+                    label.centerX == centreLabel.centerX + sin(angle) * 75.0
+                    label.centerY == centreLabel.centerY + cos(angle) * 75.0
+
+                    label.width == size.width
+                    label.height == size.height
+                }
             }
         }
     }
